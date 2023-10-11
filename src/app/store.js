e1db478dac686750685d0a29916888e9d6608c8b
Maryam-Hussain-CS <maryamhussain.cs@gmail.com>
2023-10-11 13:13:12 +0500

    Random User Generator App task Completed

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { usersApi } from "../services/users";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [usersApi.reducerPath]: usersApi.reducer,
   },
+  middleware: (getDefaultNormalizer) =>
+    getDefaultNormalizer().concat(usersApi.middleware),
 });
+
+setupListeners(store.dispatch);
+
